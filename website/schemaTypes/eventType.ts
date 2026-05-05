d0ebc17354b25140fff267b152111e73e1fec122
Logs22 <l22bitt1@gmail.com>
2026-05-05 14:27:05 -0400

    Sanity Working Actually?

---
+++ website/schemaTypes/eventType.ts
@@ -0,0 +1,17 @@
+import {defineField, defineType} from 'sanity'
+
+export const eventType = defineType({
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'photo',
+      type: 'image',
+    }),
+  ],
+})
